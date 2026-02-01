d7421294cd33a3dbefaad59cb5fb639366f17add
Godwin <afgod98@gmail.com>
2026-02-01 16:50:58 +0000

    Refined Boardly navigation with smooth screen transitions across screens

--- webClient/app/StoreProvider.tsx
+++ webClient/app/StoreProvider.tsx
@@ -2,12 +2,19 @@
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "@/redux/store";
+import { useEffect } from "react";
+import { redirect, useRouter } from "next/navigation";
+
 
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/dashboard')
+  },[router])
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
